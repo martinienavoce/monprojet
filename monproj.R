e957bc6a3068349a2df478cac78e8f98dfeda16c
AVOCE Martinien <martinienavoce@gmail.com>
2025-10-22 17:31:34 +0000

    Création de notre projet

---
+++ monproj.R
@@ -0,0 +1,7 @@
+
+usethis::use_git_config(scope = "user",
+                        user.name = "AVOCE Martinien", 
+                        user.email = "martinienavoce@gmail.com")
+
+
+gitcreds::gitcreds_set()
